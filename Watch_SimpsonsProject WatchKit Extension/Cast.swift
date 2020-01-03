24c9ac5247b9af0fed1cb149957baf3fe24b7fc6
Me <Me>
2020-01-03 05:26:38 -0800

    feat: first page table elements built

---
+++ Watch_SimpsonsProject WatchKit Extension/Cast.swift
@@ -0,0 +1,13 @@
+//
+//  Cast.swift
+//  Watch_SimpsonsProject WatchKit Extension
+//
+//  Created by dirtbag on 12/26/19.
+//  Copyright © 2019 dirtbag. All rights reserved.
+//
+
+import UIKit
+
+class Cast: NSObject {
+
+}
---
+++ Watch_SimpsonsProject WatchKit Extension/TableRowController.swift
@@ -0,0 +1,13 @@
+//
+//  TableRowController.swift
+//  Watch_SimpsonsProject WatchKit Extension
+//
+//  Created by dirtbag on 12/26/19.
+//  Copyright © 2019 dirtbag. All rights reserved.
+//
+
+import UIKit
+
+class TableRowController: NSObject {
+
+}
